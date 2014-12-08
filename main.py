ec508e51993c6086dae6eca3866def8ef5869039
Matt Broussard <mattb@cs.utexas.edu>
2014-12-08 17:13:45 -0600

    oops, this should have been in the last commit

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
+#!/usr/bin/env python
+
 import os
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__, static_url_path='')
+
+@app.route("/fbinit.js")
+def facebookInit():
+    template = """
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId: '%s',
+            xfbml: true,
+            status: true,
+            version: 'v2.2'
+        });
+        FB.getLoginStatus(window.onFBLoginCallback);
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    """
+
+    return Response(
+        template % os.environ.get("FB_APP_ID", 0),
+        mimetype='text/javascript'
+    )
 
 @app.route("/")
 def root():
